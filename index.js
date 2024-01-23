ecd50a7b59751ea64c42b711063017a587c6b5f5
arif20026 <arifislamipe026@gmail.com>
2024-01-23 21:29:50 +0600

    added post operation for jwt

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion ,ObjectId} = require('mongodb');
 require('dotenv').config()
 const app = express();
@@ -8,15 +10,16 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors(
-//     {
-//     origin: [
-//         'http://localhost:5173',
+    {
+    origin: [
+        'http://localhost:5173',
        
-//     ],
-//     credentials: true
-// }
+    ],
+    credentials: true
+}
 ));
 app.use(express.json());
+app.use(cookieParser());
 
 
 
@@ -34,6 +37,28 @@ const client = new MongoClient(uri, {
     }
 });
 
+// middlewares 
+const logger = (req, res, next) =>{
+    console.log('log: info', req.method, req.url);
+    next();
+}
+
+const verifyToken = (req, res, next) =>{
+    const token = req?.cookies?.token;
+    // console.log('token in the middleware', token);
+    // no token available 
+    if(!token){
+        return res.status(401).send({message: 'unauthorized access'})
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+        if(err){
+            return res.status(401).send({message: 'unauthorized access'})
+        }
+        req.user = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -41,6 +66,27 @@ async function run() {
         const blogCollection = client.db('blogUser').collection('blogs');
         const wishListCollection = client.db('blogUser').collection('wishList');
         const commentCollection = client.db('blogUser').collection('comments');
+
+
+        // auth realted api
+             app.post('/jwt', logger, async (req, res) => {
+            const user = req.body;
+            console.log('user for token', user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: true,
+                sameSite: 'none'
+            })
+                .send({ success: true });
+        })
+
+        app.post('/logout', async (req, res) => {
+            const user = req.body;
+            console.log('logging out', user);
+            res.clearCookie('token', { maxAge: 0 }).send({ success: true })
+        })
 
 
         app.post('/blogs', async (req, res) => {
@@ -100,7 +146,7 @@ async function run() {
             res.send(result);
         });
 
-        app.get('/wishList', async (req, res) => {
+        app.get('/wishList',logger, verifyToken, async (req, res) => {
 
             console.log(req.query.email)
             let query = {}
@@ -154,7 +200,7 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         });
-            
+
 
         // app.get('/comments/:id', async (req, res) => {
         //     const id = req.params.id;
